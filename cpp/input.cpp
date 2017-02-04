184825bdb5adb2395595cd886ed403b709f19ef4
RemoteAlgorithm <remotealgorithm@gmail.com>
2017-02-04 14:58:35 -0500

    save

---
+++ cpp/input.cpp
@@ -0,0 +1,10 @@
+//input
+#include <iostream>
+
+int main() {
+  std::cout << "Enter name" << std::endl;
+  std::string player_name;
+  std::cin >> player_name;
+  std::cout << player_name << "? Nice name!" << std::endl;
+
+}
